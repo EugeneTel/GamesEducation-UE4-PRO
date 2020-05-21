db2bc646cb9c7dee8c1530fbf8ccccf6f83b3de7
EugeneTel <eugene.telyatnik@gmail.com>
2020-05-21 12:44:23 +0200

    Initial commit

---
+++ Source/GamesEducation/GamesEducationGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GamesEducationGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMESEDUCATION_API AGamesEducationGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
